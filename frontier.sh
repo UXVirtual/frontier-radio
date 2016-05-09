c2294f41f8b6bc208ee5899d7f076ec8d864b761
Michael Andrew <michael@gladeye.co.nz>
2016-05-09 21:07:57 +1200

    Updated daemon script

--- frontier.sh
+++ frontier.sh
@@ -1,36 +1,62 @@
-#! /bin/sh
+#!/bin/sh
+
 ### BEGIN INIT INFO
 # Provides:          frontier
-# Required-Start:    $all
-# Required-Stop:
+# Required-Start:    $remote_fs $syslog
+# Required-Stop:     $remote_fs $syslog
 # Default-Start:     2 3 4 5
 # Default-Stop:      0 1 6
-# Short-Description: Manage my cool stuff
+# Short-Description: Controls Frontier Radio service
+# Description:       Allows the Frontier Radio to be started and stopped.
 ### END INIT INFO
 
-PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/opt/bin
+# Change the next 3 lines to suit where you install your script and what you want to call it
+DIR=/home/pi/frontier-radio
+DAEMON=$DIR/player.sh
+DAEMON_NAME=frontier
 
-. /lib/init/vars.sh
+# Add any command line options for your daemon here
+DAEMON_OPTS=""
+
+# This next line determines what user the script runs as.
+# Root generally not recommended but necessary if you are using the Raspberry Pi GPIO from Python.
+DAEMON_USER=pi
+
+# The process ID of the script when it runs is stored here:
+PIDFILE=/var/run/$DAEMON_NAME.pid
+
 . /lib/lsb/init-functions
 
-PID=''
+do_start () {
+    log_daemon_msg "Starting system $DAEMON_NAME daemon"
+    start-stop-daemon --start --background --pidfile $PIDFILE --make-pidfile --user $DAEMON_USER --chuid $DAEMON_USER --startas $DAEMON -- $DAEMON_OPTS
+    log_end_msg $?
+}
+do_stop () {
+    log_daemon_msg "Stopping system $DAEMON_NAME daemon"
+    start-stop-daemon --stop --pidfile $PIDFILE --retry 10
+    log_end_msg $?
+}
 
 case "$1" in
-  start)
-    log_begin_msg "Starting Frontier Radio"
-    cd /home/pi/frontier-radio
-    ./player.sh & export PID=$!
-    log_end_msg $?
-    exit 0
-    ;;
-  stop)
-    log_begin_msg "Stopping Frontier Radio"
-    kill "$PID"
-    log_end_msg $?
-    exit 0
-    ;;
-  *)
-    echo "Usage: service frontier {start|stop}"
-    exit 1
-    ;;
+
+    start|stop)
+        do_${1}
+        ;;
+
+    restart|reload|force-reload)
+        do_stop
+        do_start
+        ;;
+
+    status)
+        status_of_proc "$DAEMON_NAME" "$DAEMON" && exit 0 || exit $?
+        ;;
+
+    *)
+        echo "Usage: /etc/init.d/$DAEMON_NAME {start|stop|restart|status}"
+        exit 1
+        ;;
+
 esac
+exit 0
